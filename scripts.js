1997efc02f08a352d8d2e9f365420ccde45f0293
sljohnson32 <sljohnson32@gmail.com>
2017-10-17 22:26:26 -0600

    Add final CSS and function to handle switching

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$('.tab').click((e) => {
+  let tabID = $(e.target).parents('.tab').attr('data-tab')
+  $('.tab.is-selected').removeClass('is-selected')
+  $('.display.is-selected').removeClass('is-selected')
+  $(`[data-tab=${tabID}]`).addClass('is-selected')
+  $(`[data-display=${tabID}]`).addClass('is-selected')
+})
+
+
+
+// if ($(e.target).attr('class') == 'unlocked') {
+//   $(e.target).toggleClass('locked unlocked')
+//   $(e.target).attr('src', './imgs/locked.png')
+// } else {
+//   $(e.target).toggleClass('locked unlocked')
+//   $(e.target).attr('src', './imgs/unlocked.png')
+// }
